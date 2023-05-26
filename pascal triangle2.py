7126fc9ded52229af34455c1e6b3613e477ccba1
K-Jay9 <kjempirecode@gmail.com>
2023-05-26 15:06:07 +0300

    Pascal Triangle II problem

---
+++ pascal triangle2.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+
+def getRow(rowIndex):
+    res = []
+    for i in range(1, rowIndex+2):
+        temp = []
+        if i == 1:
+            temp.append(1)
+        else:
+            adder = 0
+            prev = res[-1]
+            if len(prev) == 1:
+                temp.append(1)
+            else:
+                for j in range(1,len(prev)):
+                    if prev[j-1] == 1:
+                        temp.append(1)
+                    adder = prev[j] +prev[j-1]
+                    temp.append(adder)
+                    adder = 0
+            temp.append(1)
+        res.append(temp)
+        temp = []
+
+    return res[rowIndex]
+
+print(getRow(3))
