667432baa62bce8f5608e75b43db22f4370b76b8
K-Jay9 <kjempirecode@gmail.com>
2023-06-14 16:55:35 +0300

    ax profit dynamic programming

---
+++ max Profit.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+def solution(prices):
+    left = 0 #Buy
+    right = 1 #Sell
+    max_profit = 0
+    while right < len(prices):
+        currentProfit = prices[right] - prices[left] #our current Profit
+        if prices[left] < prices[right]:
+            max_profit =max(currentProfit,max_profit)
+        else:
+            left = right
+        right += 1
+    return max_profit
+
+if __name__ == "__main__":
+    prices = [7,1,5,3,6,4]
+    print(solution(prices))
+    print(solution([7,6,4,3,1]))
